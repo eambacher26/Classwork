2509246bfc6a09c34f88202b4fb8707d6b4fff79
eambacher26 <ambacher4@gmail.com>
2022-09-28 18:30:06 -0800

    upload

---
+++ Madlib.py
@@ -0,0 +1,23 @@
+print("Hello, this is a holiday mad-lib")
+adj_1 = input("Please provide an adjective: ")
+adj_2 = input("Please provide an adjective: ")
+bird_type = input("Please provide a type of bird: ")
+room = input("Please provide a room in the house: ")
+verb_past = input("Please provide a past-tense verb: ")
+verb_1 = input("Please provide a verb: ")
+relative = input("Please provide a relatives name: ")
+noun_1 = input("Please provide a noun: ")
+liquid = input("Please provide a liquid: ")
+verb_ing = input("Please provide a verb ending in -ing: ")
+body_part=input("Please provide a body part: (plural)")
+noun_2=input("Please provide a plural noun: ")
+verb_ing2=input("Please provide a verb ending in -ing: ")
+noun_3=input("Please provide a noun: ")
+print("It was a " + adj_1 + " cold November day." 
+" I woke up to the " + adj_2 + " smell of " + bird_type + " roasting downstairs"
+" I " + verb_past + " down the stairs to see if I could help " + verb_1 + " the dinner."
+" My mom said, "'"'"see if " + relative + " needs a fresh " +noun_1+ " ."
+" So I carried a tray of glasses full of " + liquid + " into the " + verb_ing + " room."
+" When I got there, I couldn't believe my " + body_part +"!"
+" There were " + noun_2 + " " + verb_ing2 + " on the " + noun_3 +"!")
+
