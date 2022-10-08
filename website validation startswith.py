741c1da4b17dcbff98563a9835aaa09567d2a978
eambacher26 <ambacher4@gmail.com>
2022-10-08 11:15:25 -0800

    website http validator

---
+++ website validation startswith.py
@@ -0,0 +1,7 @@
+
+website=input("give me a website: ")
+result = website.startswith("http://")
+if result == False:
+    print("this is not a valid website.")
+elif result == True:
+    print("thank you!")
