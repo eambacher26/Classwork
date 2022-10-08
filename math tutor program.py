f268da648055474060d9bf89cfe4b98536294e8d
eambacher26 <ambacher4@gmail.com>
2022-10-08 11:14:29 -0800

    Final with random problem gener.

--- math tutor program.py
+++ math tutor program.py
@@ -4,8 +4,10 @@ score = 0                        # need to assign base value to score
 for i in range(11):                 # means it will generate ten questions need 11 for 10 questions?
     int_a = random.randint(1,10)                # will be first number
     int_b = random.randint(1,10)                #will be second number
-    operators = ['+', '-', '*'] #need to find out how to round for division that results in decimals to get correct answer
+    operators = ['+', '-', '*', '/'] #need to find out how to round for division that results in decimals to get correct answer
     operator_value = random.choice(operators)
+    if operator_value == "/":
+        int_a = int_a * int_b
     question = str(int_a) + ' ' +str(operator_value) + ' ' + str(int_b) #will print question in string form for user
     answer = eval(question)
     question+= ': '             # adds colon/ string to question formula
@@ -19,5 +21,6 @@ for q in questions.keys():              # q can be any variable, it is temporary
         print("Correct!")             #the .get function returns key values
         score=score+1       #this will assign one to score variable in case of right answer to be printed out at end
     else:
-        print("Incorrect!")    
+        print("Incorrect!")
+        print(questions.get(q))
 print("You got " + str(score) + " correct!") #important to set score as str, otherwise it is an int and has print problems.
